ece302ae341a7a9437d76776d71245af8620c0f3
yerimbae <rimyebae@naver.com>
2023-09-12 13:18:55 +0900

    강의 예제 실습

---
+++ src/chapter_14/DarkOrLight.jsx
@@ -0,0 +1,24 @@
+import {useState, useContext, useCallback} from "react";
+import ThemeContext from "./ThemeContext";
+import MainContent from "./MainContext";
+
+function DarkOrLight(props) {
+    const [theme, setTheme] = useState("light");
+
+    const toggleTheme = useCallback(() => {
+        if(theme == "light") {
+            setTheme("dark");
+        }else if (theme == "dark") {
+            setTheme("light");
+        }
+    },[theme]);
+
+    return(
+        <ThemeContext.Provider value={{theme,toggleTheme}}>
+            <MainContent />
+        </ThemeContext.Provider>
+    );
+
+}
+
+export default DarkOrLight;
